c00bf72bc3f866efbba9f60950920c0cabad9df6
Esahtengang Asonganyi <93486453+Esahte@users.noreply.github.com>
2023-11-09 12:16:47 -0400

    Added comment to the files add refactored the styles.css file for more readability

--- ajax-superheroes-master/app.js
+++ ajax-superheroes-master/app.js
@@ -1,28 +1,30 @@
-"use strict";
-window.onload = function() {
-    // Write code that allows the user to search for a superhero and displays the results
-    const searchButton = document.getElementById("search-btn");
-    const searchInput = document.getElementById("search-box");
-    let httpRequest;
+"use strict"; // Enforces strict mode to catch common JavaScript pitfalls
 
-    searchButton.onclick = function(element) {
-        element.preventDefault();
+window.onload = function() { // This function will run after the page has finished loading
+    const searchButton = document.getElementById("search-btn"); // Get the search button element
+    const searchInput = document.getElementById("search-box"); // Get the search input element
+    let httpRequest; // Declare a variable for the HTTP request
 
-        httpRequest = new XMLHttpRequest();
+    searchButton.onclick = function(element) { // This function will run when the search button is clicked
+        element.preventDefault(); // Prevent the default form submission behavior
+
+        httpRequest = new XMLHttpRequest(); // Create a new HTTP request
 
         // GET Request
-        httpRequest.onreadystatechange = function() {
-            if (httpRequest.readyState === XMLHttpRequest.DONE) {
-                if (httpRequest.status === 200) {
-                    let response = httpRequest.responseText;
-                    const searchResult = document.getElementById("results");
-                    searchResult.innerHTML = response;
-                } else {
-                    alert("There was a problem with the request.");
+        httpRequest.onreadystatechange = function() { // This function will run whenever the readyState property changes
+            if (httpRequest.readyState === XMLHttpRequest.DONE) { // The request has been completed
+                if (httpRequest.status === 200) { // The request was successful
+                    let response = httpRequest.responseText; // Get the response text
+                    const searchResult = document.getElementById("results"); // Get the results element
+                    searchResult.innerHTML = response; // Insert the response text into the results element
+                } else { // The request was not successful
+                    alert("There was a problem with the request."); // Alert the user about the problem
                 }
             }
         };
-        httpRequest.open("GET", "superheroes.php?query=" + searchInput.value, true);
-        httpRequest.send();
+        const url = "superheroes.php?query=" + encodeURIComponent(searchInput.value); // Encode the input value to safely include it in the URL
+        httpRequest.open("GET", url, true); // Initialize the request
+        httpRequest.send(); // Send the request
     }
 };
+
